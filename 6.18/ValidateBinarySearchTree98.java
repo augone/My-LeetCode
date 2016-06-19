a1ceafb162816cc7594fd1e1e8938ef51bc55d2b
Xiaodong Ma <augustdoker@gmail.com>
2016-06-18 22:06:58 -0400

    Add another method to problem 98. Validate Binary Search Tree;

--- 6.18/ValidateBinarySearchTree98.java
+++ 6.18/ValidateBinarySearchTree98.java
@@ -18,3 +18,115 @@ public class Solution {
 		return isValidBST(root.right);
     }
 }
+
+
+/**This is another method, a little bit slower
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    class Info
+	{
+	    boolean isNull;
+		int max;
+		int min;
+		boolean b;
+		
+	}
+	
+    public boolean isValidBST(TreeNode root) {
+        Info info = goCheck(root);
+		return info.b;
+    }
+	public Info goCheck(TreeNode root)
+	{
+		Info info = new Info();
+		if(root == null)
+		{
+		    info.isNull = true;
+			info.min = Integer.MAX_VALUE;
+			info.max = Integer.MIN_VALUE;
+			info.b = true;
+			return info;
+		}
+		info.isNull = false;
+		Info l = goCheck(root.left);
+		if(l.b==false)
+		{
+			info.b = false;
+			return info;
+		}
+		Info r = goCheck(root.right);
+		if(r.b==false)
+		{
+			info.b = false;
+			return info;
+		}
+		if(root.val == Integer.MAX_VALUE)
+		{
+		    if(l.isNull)
+		    {
+		        if(!r.isNull)
+		        {
+	                info.b = false;
+	                return info;
+		        }
+		        
+		    }
+		    else
+		    {
+		        if(l.max == root.val||!r.isNull)
+		        {
+		            info.b = false;
+		            return info;
+		        }
+		    }
+		    info.b = true;
+	        info.min = Math.min(l.min,root.val);
+	        info.max = root.val;
+	        return info;
+		}
+		else if(root.val == Integer.MIN_VALUE)
+		{
+		    if(l.isNull)
+		    {
+		        if(!r.isNull)
+		        {
+		            if(r.min == root.val)
+		            {
+		                info.b = false;
+		                return info;
+		            }
+		            
+		        }
+		        
+		    }
+		    else
+		    {
+		        
+		            info.b = false;
+		            return info;
+		        
+		    }
+		    info.b =true;
+	        info.min = root.val;
+	        info.max = r.max;
+	        return info;
+		}
+		else if(root.val<=l.max||root.val>=r.min)
+		{
+			info.b =false;
+			return info;
+		}
+			
+		info.b = true;
+		info.min = Math.min(l.min,root.val);
+		info.max = Math.max(r.max,root.val);
+		return info;
+	}
+}
