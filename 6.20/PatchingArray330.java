c13d88a578382fd42e3fe0829d8e914c6a2f7757
Xiaodong Ma <augustdoker@gmail.com>
2016-06-20 11:47:05 -0400

    A better solution for problem 330. Patching Array;

--- 6.20/PatchingArray330.java
+++ 6.20/PatchingArray330.java
@@ -148,3 +148,30 @@ class Solution {
     }
 	
 }
+
+/**A supper good solution
+public class Solution {
+    public int minPatches(int[] nums, int n) {
+        int cnt;
+        int patches = 0;
+        long nextToMaxSum = 1;
+
+        for(cnt = 0; cnt<nums.length && nextToMaxSum-1<n; ){
+            if(nextToMaxSum < nums[ cnt ]){ 
+                nextToMaxSum = nextToMaxSum << 1;
+                patches++;
+            } else {
+                nextToMaxSum += nums[ cnt++ ];
+            }
+        }
+
+        while(nextToMaxSum-1 < n){
+            nextToMaxSum = nextToMaxSum << 1;
+            patches++;
+        }
+
+        return patches;
+    }
+}
+
+*/
