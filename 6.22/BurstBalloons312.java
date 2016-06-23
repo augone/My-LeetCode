693912315cb60a96b47937f8ca05485e2bc4497f
Xiaodong Ma <augustdoker@gmail.com>
2016-06-22 21:38:03 -0400

    Completed problem 312. Burst Balloons;

--- 6.22/BurstBalloons312.java
+++ 6.22/BurstBalloons312.java
@@ -1,3 +1,31 @@
+/* This is the kind of problem we use dynamic programming. WHY? 
+it's very challenging to figure out what's the pattern of optimal 
+burst order. In fact, there's no clear rule that makes sense. Shall 
+we burst the balloon with maximum coins? Or shall we burst the one 
+with least. This is the time we introduce Dynamic Programming, as we 
+want to solve the big problem from small subproblem. It is clear that 
+the amount of coins you gain relies on your previous steps. This is a 
+clear signal of using DP.
+
+The hard part is to define the subproblem. Think out what is clear 
+in this problem? Let's scale this problem down. What is the fact you 
+know for sure? Say if the array has only 1 balloon. The maximum coin 
+would be the coin inside this ballon. This is the starting point! So 
+let's move on to array with 2 balloons. Here, we have 2 cases, which 
+of the balloon is the last one. The last one times the coins in 
+boundary is the gain we get in the end. That is to say, last balloon 
+is the key. Since we don't know the pattern of optimal. We just blindly 
+iterate each balloon and check what's total gain if it's the last ballon.
+
+
+Let's use dp[i][j] to denote maximum gain from balloon range i to j. 
+We try out each balloon as last burst in this range. Then the 
+subproblem relation would be:
+
+foreach k in i to j: dp[j][i] = max(array[j-1]array[k]array[i+1] + 
+dp[j][k-1] + dp[k+1][i], dp[j][i]); */
+
+
 import java.util.*;
 class Solution {
 	public static void main(String[] args )
@@ -26,9 +54,9 @@ class Solution {
 			{
 				for(int k = l; k <= r; k++)
 				{
-					sop("dp["+l+"]["+r+"]:"+dp[l][r]);
+					//sop("dp["+l+"]["+r+"]:"+dp[l][r]);
 					dp[l][r] = Math.max(dp[l][r],rep[l-1]*rep[k]*rep[r+1]+dp[k+1][r]+dp[l][k-1]);
-					sop("after : dp["+l+"]["+r+"]:"+dp[l][r]);
+					//sop("after : dp["+l+"]["+r+"]:"+dp[l][r]);
 				}
 			}
 		}
